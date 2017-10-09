a9994694e2aa1ac979f12237eea3beef9980e499
George-R <george.redak91@gmail.com>
2017-10-09 13:30:43 -0400

    ps-01

--- PS01/index.js
+++ PS01/index.js
@@ -1,3 +1,86 @@
-var svg = d3.select('svg');
+var svg = d3.select('svg')
+.append('g')
+.attr('transform', "translate(100,100)");
+var allData;
 
-/* Your code goes here */
+var scaleX = d3.scaleLinear().domain([0,30]).range([0,600]);
+var scaleY = d3.scaleLinear().domain([60,0]).range([0,400]);
+
+var currentYear = "James";
+
+d3.csv('data.csv', function(dataIn){
+
+  allData = dataIn
+
+  var data2016 = dataIn.filter(function(d){
+  return d.year =="James";
+
+});
+
+svg.selectAll('circle')
+  .data(data2016)
+  .enter()
+  .append('circle')
+  .attr('class', 'myCircles');
+
+svg.append('g')
+  .attr('transform', 'translate (0,400)')
+  .call(d3.axisBottom(scaleX));
+
+svg.append('g')
+  .attr('transform', 'translate (0,0) ')
+  .call(d3.axisLeft(scaleY));
+
+
+
+updateData(data2016);
+
+});
+
+function updateData(dataPoints) {
+
+  svg.selectAll('.myCircles')
+    .data(dataPoints)
+    .attr('cx', function(d){
+    return scaleX(d.x);
+
+
+  })
+    .attr('cy', function(d){
+    return scaleY(d.y);
+
+  })
+    .attr('r', function(d){
+    return d.r;
+
+  })
+
+};
+
+function buttonClicked(){
+  if (currentYear == "James") {
+    var data2017 = allData.filter(function(d){
+     return d.year == "Curry";
+});
+
+
+console.log(data2017)
+currentYear = "Curry";
+updateData(data2017);
+
+}
+else {
+
+data2016 = allData.filter(function(d){
+  return d.year == "James";
+});
+console.log(data2016)
+currentYear = "James";
+updateData(data2016);
+}
+
+
+}
+window.setInterval(function(){
+  buttonClicked()
+}, 3000)
--- makeButton/index.js
+++ makeButton/index.js
@@ -1,11 +1,55 @@
-var svg = d3.select('svg');
+var svg = d3.select('svg')
+  .append('g')
+  .attr('transform', "translate(100,100)");
+
+
+
+d3.csv('data.csv', function(d){
+console.log(d);
+
+
+svg.selectAll('circle')
+  .data(d)
+  .enter()
+  .append('circle')
+  .attr('cx', function(d){
+    return scaleX(d.x)
+  })
+  .attr('cy', function(d){
+    return scaleY(d.y)
+  })
+  .attr('r', 10);
+
+svg.append('g')
+  .attr('transform', 'translate(0,400)')
+  .call(d3.axisBottom(scaleX));
+
+svg.append('g')
+  .call(d3.axisLeft(scaleY));
+
+svg.append('text')
+  .attr('x', 300)
+  .attr('y', 0)
+  .attr('font-size', 24)
+  .text('Test')
+
+svg.append('text')
+    .attr('x', 300)
+    .attr('y', 450)
+    .attr('font-size', 16)
+    .text('x axis title')
+
+svg.append('text')
+      .attr('transform', 'rotate(270)')
+      .attr('x', -100)
+      .attr('y', -50)
+      .attr('font-size', 16)
+      .text('y title axis')
+
+})
+
+var scaleX = d3.scaleLinear().domain([0,400]).range([500,0]);
+var scaleY = d3.scaleLinear().domain([0,400]).range([0,400]);
+
 
 /* Your code goes here */
-
-
-function buttonClicked(){
-    //do something
-    console.log('here');
-}
-
-
